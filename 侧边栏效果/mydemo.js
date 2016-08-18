875a9cbcf61a7294d15f88c097f7fc212514b967
MzPathing <13242362600@163.com>
2016-08-18 21:45:39 +0800

    重写css样式和js代码，用js实现动画效果而不是css3以此解决ie低版本不兼容问题

--- 侧边栏效果/mydemo.js
+++ 侧边栏效果/mydemo.js
@@ -24,18 +24,19 @@ window.onload=function (){
     var ifshow=true;
     var now=2;
     var deg=0;
+    var oSideBar=document.getElementById("sidebar");
     oCloseBar.onclick=function (){
         if(ifshow){
             for(var i=0,len=oClose.length;i<len;i++){
                 move(oContent[i],jsonHidden);
             }
-            move(oItem[0].parentNode.parentNode,hidden);
+            move(oSideBar,hidden);
             move(oCloseBar,icongo);
             var timer=setInterval(function (){
                 if(parseInt(oCloseBar.style.marginLeft)>=200){
                     clearInterval(timer);
                 }
-                rotate(oCloseBar)
+                rotate(oCloseBar);
             },100);
             ifshow=false;
         }
@@ -90,14 +91,20 @@ window.onload=function (){
                     var value=parseInt(getStyle(obj,attr));
                  }
                 var speed=(json[attr]-value)/10;//(100-30)/10=7
-                speed=speed>0?Math.ceil(speed):Math.floor(speed);
-                if(attr=="opacity"){
-                    obj.style.filter="alpha(opacity="+(speed+value)+")";
-                    obj.style.opacity=(value+speed)/100;
+                if(speed==0){
+                	clearInterval(obj.timer);
                 }
                 else{
-                    obj.style[attr]=speed+value+"px";
+					speed=speed>0?Math.ceil(speed):Math.floor(speed);
+	                if(attr=="opacity"){
+	                    obj.style.filter="alpha(opacity="+(speed+value)+")";
+	                    obj.style.opacity=(value+speed)/100;
+	                }
+	                else{
+	                    obj.style[attr]=speed+value+"px";
+	                }
                 }
+                
             }    
         },30)
     }
